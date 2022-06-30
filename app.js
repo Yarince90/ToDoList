16dd6f692ee60536061438bfeafc362237112b67
Yarince Vigil <Yarincevigil@hotmail.com>
2022-06-30 12:03:10 -0400

    Finilized main functionality for to-do list

--- app.js
+++ app.js
@@ -2,12 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+let items = [];
+
 app.get('/', (req, res)=>{
-    res.send("Server is Up and Running! :)");
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-us", options);
+    res.render('list.ejs', {kindOfDay: day, newListItems: items});
 });
+
+app.post('/', (req, res)=>{
+    let item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect('/');
+})
+
 
 app.listen(port, ()=>{
     console.log("Server is started on port: " + port);
